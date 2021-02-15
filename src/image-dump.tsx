bbfce628da26ce6a4109e483726fc4e2aab3b8e3
Ewan Lyon <clubwhomc@gmail.com>
2021-02-15 12:03:26 +1100

    Added nav bar image

--- src/image-dump.tsx
+++ src/image-dump.tsx
@@ -3,7 +3,10 @@ import { render } from 'react-dom';
 
 const ImageDump: React.FC = () => {
 	return (
-		<img src={require('./media/random/iPhone X - Silver.png')}/>
+		<>
+			<img src={require('./media/random/iPhone X - Silver.png')} />
+			<img src={require('./media/random/TwitterNavBar.png')} />
+		</>
 	);
 };
 
