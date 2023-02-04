b52f1cc60c81dc53a9963ef6290e2e9dd8a2f4c2
Ewan Lyon <websites@ewanlyon.com>
2023-02-04 23:35:30 +1100

    change to vercel

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,12 +3,12 @@ import tailwind from '@astrojs/tailwind';
 import react from "@astrojs/react";
 
 // https://astro.build/config
-import netlify from "@astrojs/netlify/functions";
+import vercel from "@astrojs/vercel/serverless";
 
 // https://astro.build/config
 export default defineConfig({
   integrations: [tailwind(), react()],
   site: 'https://ewanlyon.com/',
   output: "server",
-  adapter: netlify()
+  adapter: vercel()
 });
