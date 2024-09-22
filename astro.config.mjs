4abe3e20e69aaa4ce024e1b1f26603420489cc3f
Ewan Lyon <websites@ewanlyon.com>
2024-09-22 22:50:31 +1000

    git intro post

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,22 +2,20 @@ import { defineConfig } from 'astro/config';
 import tailwind from '@astrojs/tailwind';
 import react from '@astrojs/react';
 
-// https://astro.build/config
 import vercel from '@astrojs/vercel/serverless';
 
 // https://astro.build/config
 import sitemap from '@astrojs/sitemap';
 
 // https://astro.build/config
+import mdx from "@astrojs/mdx";
+
+// https://astro.build/config
 export default defineConfig({
-	integrations: [
-		tailwind(),
-		react(),
-		sitemap({
-			filter: (page) => page !== 'https://ewanlyon.com/riv',
-		}),
-	],
-	site: 'https://ewanlyon.com/',
-	output: 'server',
-	adapter: vercel(),
+  integrations: [tailwind(), react(), sitemap({
+    filter: page => page !== 'https://ewanlyon.com/riv'
+  }), mdx()],
+  site: 'https://ewanlyon.com/',
+  output: 'server',
+  adapter: vercel()
 });
